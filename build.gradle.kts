571607e86bcc53708408ae0834c01f03dc2cf71a
sliman4 <4sliman4@gmail.com>
2022-10-17 17:57:11 +0300

    Fix indentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-	java
+    java
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -19,15 +19,15 @@ dependencies {
 
 subprojects {
     repositories {
-		mavenCentral()
+        mavenCentral()
         maven("https://jitpack.io")
         maven("https://papermc.io/repo/repository/maven-public/")
     }
-	
-	dependencies {
-		compileOnly("com.github.4drian3d:MiniPlaceholders:1.3.1")
+
+    dependencies {
+        compileOnly("com.github.4drian3d:MiniPlaceholders:1.3.1")
         compileOnly("net.luckperms:api:5.4")
-	}
+    }
 }
 
 tasks {
