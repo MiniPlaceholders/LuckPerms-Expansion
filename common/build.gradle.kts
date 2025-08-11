46f6666d69efc8a76f096cac065bc047f4835a28
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-08-11 17:37:06 -0500

    feat!: Updated to MiniPlaceholders 3

--- common/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    compileOnly(libs.miniplaceholders)
-    compileOnly(libs.adventure.api)
-    compileOnly(libs.adventure.minimessage)
-    compileOnly(libs.adventure.legacy)
-    compileOnly(libs.luckperms)
-}
--- src/main/java/io/github/miniplaceholders/expansion/luckperms/LuckPermsExpansionProvider.java
+++ src/main/java/io/github/miniplaceholders/expansion/luckperms/LuckPermsExpansionProvider.java
@@ -1,13 +1,16 @@
-package io.github.miniplaceholders.expansion.luckperms.common;
+package io.github.miniplaceholders.expansion.luckperms;
 
 import io.github.miniplaceholders.api.Expansion;
-import io.github.miniplaceholders.api.utils.TagsUtils;
+import io.github.miniplaceholders.api.provider.ExpansionProvider;
+import io.github.miniplaceholders.api.provider.LoadRequirement;
+import io.github.miniplaceholders.api.utils.Tags;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryOptions;
@@ -16,46 +19,59 @@ import net.luckperms.api.util.Tristate;
 import java.util.Optional;
 import java.util.UUID;
 
-import static io.github.miniplaceholders.api.utils.Components.*;
-import static io.github.miniplaceholders.api.utils.LegacyUtils.*;
+import static io.github.miniplaceholders.api.utils.Components.FALSE_COMPONENT;
+import static io.github.miniplaceholders.api.utils.Components.TRUE_COMPONENT;
+import static io.github.miniplaceholders.api.utils.LegacyStrings.parsePossibleLegacy;
 
-public record CommonExpansion(LuckPerms luckPerms) {
+public final class LuckPermsExpansionProvider implements ExpansionProvider {
     private static final Component UNDEFINED_COMPONENT = Component.text("undefined", NamedTextColor.GRAY);
 
-    public Expansion.Builder commonBuilder() {
+    private User user(final Audience audience, final LuckPerms luckPerms) {
+        final UUID uuid = audience.get(Identity.UUID).orElse(null);
+        if (uuid == null) {
+            return null;
+        }
+        return luckPerms.getUserManager().getUser(uuid);
+    }
+
+    @Override
+    public Expansion provideExpansion() {
+        final LuckPerms luckPerms = LuckPermsProvider.get();
         return Expansion.builder("luckperms")
+                .author("MiniPlaceholders Contributors")
+                .version("2.0.0")
                 .audiencePlaceholder("prefix", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
                     return Tag.inserting(parsePossibleLegacy(user.getCachedData().getMetaData().getPrefix(), ctx));
                 })
                 .audiencePlaceholder("suffix", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
                     return Tag.inserting(parsePossibleLegacy(user.getCachedData().getMetaData().getSuffix(), ctx));
                 })
                 .audiencePlaceholder("meta", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
                     final String meta = queue.popOr(() -> "you need to introduce a meta key").value();
-                    final Optional<QueryOptions> queryOptions = this.luckPerms.getContextManager().getQueryOptions(user);
+                    final Optional<QueryOptions> queryOptions = luckPerms.getContextManager().getQueryOptions(user);
                     if (queryOptions.isEmpty()) {
                         return null;
                     }
                     final String result = user.getCachedData().getMetaData(queryOptions.get()).getMetaValue(meta);
                     if (result == null) {
-                        return TagsUtils.EMPTY_TAG;
+                        return Tags.EMPTY_TAG;
                     }
                     return Tag.preProcessParsed(result);
                 })
                 .audiencePlaceholder("has_permission", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
@@ -67,20 +83,20 @@ public record CommonExpansion(LuckPerms luckPerms) {
                     );
                 })
                 .audiencePlaceholder("check_permission", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
                     final String permission = queue.popOr(() -> "you need to introduce an permission").value();
                     final Tristate result = user.getCachedData().getPermissionData().checkPermission(permission);
-                    return Tag.selfClosingInserting(switch(result) {
+                    return Tag.selfClosingInserting(switch (result) {
                         case TRUE -> TRUE_COMPONENT;
                         case FALSE -> FALSE_COMPONENT;
                         case UNDEFINED -> UNDEFINED_COMPONENT;
                     });
                 })
                 .audiencePlaceholder("inherited_groups", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
@@ -90,18 +106,18 @@ public record CommonExpansion(LuckPerms luckPerms) {
                     return Tag.selfClosingInserting(groups);
                 })
                 .audiencePlaceholder("primary_group_name", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
                     final String primaryGroup = user.getCachedData().getMetaData().getPrimaryGroup();
                     if (primaryGroup == null) {
-                        return TagsUtils.EMPTY_TAG;
+                        return Tags.EMPTY_TAG;
                     }
                     return Tag.preProcessParsed(primaryGroup);
                 })
                 .audiencePlaceholder("inherits_group", (aud, queue, ctx) -> {
-                    final User user = user(aud);
+                    final User user = user(aud, luckPerms);
                     if (user == null) {
                         return null;
                     }
@@ -110,14 +126,11 @@ public record CommonExpansion(LuckPerms luckPerms) {
                             ? TRUE_COMPONENT
                             : FALSE_COMPONENT
                     );
-                });
+                }).build();
     }
 
-    private User user(final Audience audience) {
-        final UUID uuid = audience.get(Identity.UUID).orElse(null);
-        if (uuid == null) {
-            return null;
-        }
-        return luckPerms.getUserManager().getUser(uuid);
+    @Override
+    public LoadRequirement loadRequirement() {
+        return LoadRequirement.requiredComplement("luckperms", "LuckPerms");
     }
 }
